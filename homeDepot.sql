dcfaf4eb75037fc9e0287fa6833802d9210631ed
Eduardo Luna <Lalo@MacBook-Pro-de-Eduardo-7.local>
2017-01-26 19:48:53 -0600

    Diagrama y Dump

---
+++ homeDepot.sql
@@ -0,0 +1,378 @@
+-- MySQL dump 10.16  Distrib 10.1.10-MariaDB, for osx10.6 (i386)
+--
+-- Host: localhost    Database: avanzadasTarea
+-- ------------------------------------------------------
+-- Server version	10.1.10-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cashier`
+--
+
+DROP TABLE IF EXISTS `cashier`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cashier` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL,
+  `manager` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `manager` (`manager`),
+  CONSTRAINT `cashier_ibfk_1` FOREIGN KEY (`manager`) REFERENCES `manager` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cashier`
+--
+
+LOCK TABLES `cashier` WRITE;
+/*!40000 ALTER TABLE `cashier` DISABLE KEYS */;
+INSERT INTO `cashier` VALUES (1,'Jota Pe',1),(2,'Ale Tovar',2),(3,'Enrique Lozada',3),(4,'Julian Huerta',4),(5,'Fernando Castillo',5),(6,'Jorge Beauregard',6);
+/*!40000 ALTER TABLE `cashier` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cashierSale`
+--
+
+DROP TABLE IF EXISTS `cashierSale`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cashierSale` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `cashier` int(11) DEFAULT NULL,
+  `sale` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `cashier` (`cashier`),
+  KEY `sale` (`sale`),
+  CONSTRAINT `cashiersale_ibfk_1` FOREIGN KEY (`cashier`) REFERENCES `cashier` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `cashiersale_ibfk_2` FOREIGN KEY (`sale`) REFERENCES `sale` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cashierSale`
+--
+
+LOCK TABLES `cashierSale` WRITE;
+/*!40000 ALTER TABLE `cashierSale` DISABLE KEYS */;
+INSERT INTO `cashierSale` VALUES (1,1,1),(2,1,2),(3,1,4),(4,2,2),(5,2,3),(6,2,8),(7,3,1),(8,3,6),(9,4,4),(10,5,2),(11,6,3),(12,6,8);
+/*!40000 ALTER TABLE `cashierSale` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'Woods'),(2,'Metals'),(3,'Ceramics'),(4,'Tools');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `client` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL,
+  `tel` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `client`
+--
+
+LOCK TABLES `client` WRITE;
+/*!40000 ALTER TABLE `client` DISABLE KEYS */;
+INSERT INTO `client` VALUES (1,'Clark Kent','222435465'),(2,'Steve Rogers','444345465'),(3,'Bruce Banner','232437655'),(4,'Tony Stark','2223064987'),(5,'Bruce Wayne','2221242524'),(6,'Peter Parker','2223098877');
+/*!40000 ALTER TABLE `client` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `department`
+--
+
+DROP TABLE IF EXISTS `department`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `department` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `department`
+--
+
+LOCK TABLES `department` WRITE;
+/*!40000 ALTER TABLE `department` DISABLE KEYS */;
+INSERT INTO `department` VALUES (1,'Gardnery'),(2,'Carpentry'),(3,'Toilets'),(4,'Kitchen');
+/*!40000 ALTER TABLE `department` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inventory`
+--
+
+DROP TABLE IF EXISTS `inventory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `inventory` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `product` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `product` (`product`),
+  CONSTRAINT `inventory_ibfk_1` FOREIGN KEY (`product`) REFERENCES `product` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inventory`
+--
+
+LOCK TABLES `inventory` WRITE;
+/*!40000 ALTER TABLE `inventory` DISABLE KEYS */;
+INSERT INTO `inventory` VALUES (1,1,53),(2,2,324),(3,3,422),(4,4,123),(5,5,232),(6,6,32),(7,7,132),(8,8,12);
+/*!40000 ALTER TABLE `inventory` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `manager`
+--
+
+DROP TABLE IF EXISTS `manager`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `manager` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL,
+  `tel` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `manager`
+--
+
+LOCK TABLES `manager` WRITE;
+/*!40000 ALTER TABLE `manager` DISABLE KEYS */;
+INSERT INTO `manager` VALUES (1,'Daniel Aguiñaga','222435465'),(2,'Zuramy Serrano','444345465'),(3,'Javier Santamaria','232437655'),(4,'Yael Toriz','2223064987'),(5,'Kanya Carrasco','2221242524'),(6,'Santiago Vargas','2223098877');
+/*!40000 ALTER TABLE `manager` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `department` int(11) DEFAULT NULL,
+  `category` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `department` (`department`),
+  KEY `category` (`category`),
+  CONSTRAINT `product_ibfk_1` FOREIGN KEY (`department`) REFERENCES `department` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `product_ibfk_2` FOREIGN KEY (`category`) REFERENCES `category` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,'Hammer',54,2,4),(2,'Water Centrifuger',1200,3,3),(3,'Shovel',154,1,2),(4,'Garden Razor',5000,1,4),(5,'Chainsaw',54000,2,4),(6,'Drill',1500,2,2),(7,'Fauce',4000,4,3),(8,'Trash Can',50,4,2);
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `provider`
+--
+
+DROP TABLE IF EXISTS `provider`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `provider` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL,
+  `tel` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `provider`
+--
+
+LOCK TABLES `provider` WRITE;
+/*!40000 ALTER TABLE `provider` DISABLE KEYS */;
+INSERT INTO `provider` VALUES (1,'Mike Tyson','222435465'),(2,'Samuel L. Jackson','444345465'),(3,'Bradly Cooper','232437655'),(4,'Brad Pitt','2223064987'),(5,'Peter Sam Jason','2221242524'),(6,'Papi Di Caprio','2223098877');
+/*!40000 ALTER TABLE `provider` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `purchase`
+--
+
+DROP TABLE IF EXISTS `purchase`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `purchase` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `manager` int(11) DEFAULT NULL,
+  `provider` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `manager` (`manager`),
+  KEY `provider` (`provider`),
+  CONSTRAINT `purchase_ibfk_1` FOREIGN KEY (`manager`) REFERENCES `manager` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `purchase_ibfk_2` FOREIGN KEY (`provider`) REFERENCES `provider` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `purchase`
+--
+
+LOCK TABLES `purchase` WRITE;
+/*!40000 ALTER TABLE `purchase` DISABLE KEYS */;
+INSERT INTO `purchase` VALUES (1,1,2),(2,2,3),(3,3,4),(4,4,5),(5,5,6),(6,6,1);
+/*!40000 ALTER TABLE `purchase` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `purchaseProduct`
+--
+
+DROP TABLE IF EXISTS `purchaseProduct`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `purchaseProduct` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `purchase` int(11) DEFAULT NULL,
+  `product` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `purchase` (`purchase`),
+  KEY `product` (`product`),
+  CONSTRAINT `purchaseproduct_ibfk_1` FOREIGN KEY (`purchase`) REFERENCES `purchase` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `purchaseproduct_ibfk_2` FOREIGN KEY (`product`) REFERENCES `product` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `purchaseProduct`
+--
+
+LOCK TABLES `purchaseProduct` WRITE;
+/*!40000 ALTER TABLE `purchaseProduct` DISABLE KEYS */;
+INSERT INTO `purchaseProduct` VALUES (1,1,1),(2,1,2),(3,1,4),(4,2,2),(5,2,3),(6,2,8),(7,3,1),(8,3,6),(9,4,4),(10,5,2),(11,6,3),(12,6,8);
+/*!40000 ALTER TABLE `purchaseProduct` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sale`
+--
+
+DROP TABLE IF EXISTS `sale`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sale` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `client` int(11) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `client` (`client`),
+  CONSTRAINT `sale_ibfk_1` FOREIGN KEY (`client`) REFERENCES `client` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sale`
+--
+
+LOCK TABLES `sale` WRITE;
+/*!40000 ALTER TABLE `sale` DISABLE KEYS */;
+INSERT INTO `sale` VALUES (1,1,'0000-00-00 00:00:00'),(2,1,'0000-00-00 00:00:00'),(3,2,'0000-00-00 00:00:00'),(4,3,'0000-00-00 00:00:00'),(5,4,'0000-00-00 00:00:00'),(6,5,'0000-00-00 00:00:00'),(7,6,'0000-00-00 00:00:00'),(8,5,'0000-00-00 00:00:00'),(9,6,'0000-00-00 00:00:00');
+/*!40000 ALTER TABLE `sale` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `saleProduct`
+--
+
+DROP TABLE IF EXISTS `saleProduct`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `saleProduct` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `product` int(11) DEFAULT NULL,
+  `sale` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `product` (`product`),
+  KEY `sale` (`sale`),
+  CONSTRAINT `saleproduct_ibfk_1` FOREIGN KEY (`product`) REFERENCES `product` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `saleproduct_ibfk_2` FOREIGN KEY (`sale`) REFERENCES `sale` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `saleProduct`
+--
+
+LOCK TABLES `saleProduct` WRITE;
+/*!40000 ALTER TABLE `saleProduct` DISABLE KEYS */;
+INSERT INTO `saleProduct` VALUES (1,1,1),(2,1,2),(3,1,4),(4,2,2),(5,2,3),(6,2,8),(7,3,1),(8,3,6),(9,4,4),(10,5,2),(11,6,3),(12,6,8);
+/*!40000 ALTER TABLE `saleProduct` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-01-17  1:04:51
