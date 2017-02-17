1b7de267ef3f81aa938818fdd38af384e1729710
Juan Pablo Flores Galindo <juanfg@outlook.com>
2017-02-17 12:47:34 -0600

    Se anadio la imagen y la descripcion

--- index.php
+++ index.php
@@ -7,13 +7,19 @@
 	</head>
 
 	<body>
-        <h1>Home Depot</h1>
-        <p>
-            <a href="sales.php" class="btn btn-success">Sales</a>
-            <a href="purchase.php" class="btn btn-success">Purchases</a>
-            <a href="inventory.php" class="btn btn-success">Inventory</a>
-            <a href="lastSalesPerHour.php" class="btn btn-success">Amount of Sales Per Hour</a>
-            <a href="purchaseOrders.php" class="btn btn-success">Purchase orders</a>
-		</p>
+        <div align="center">
+            <h1>Home Depot</h1><br>
+            <img src="img/hp.jpg">
+            <br><br>
+            This site is a manage system of one branch office of the popular <br>commerce called Home Depot, in this site you can make several actions like purchase <br> of new products and also you can keep track of the sales that the branch has done<br> and also the system notifies the branch when a certain product reach 0 in their inventory.
+            <br><br>
+            <p>
+                <a href="sales.php" class="btn btn-success">Sales</a>
+                <a href="purchase.php" class="btn btn-success">Purchases</a>
+                <a href="inventory.php" class="btn btn-success">Inventory</a>
+                <a href="lastSalesPerHour.php" class="btn btn-success">Amount of Sales Per Hour</a>
+                <a href="purchaseOrders.php" class="btn btn-success">Purchase orders</a>
+            </p>
+        </div>
 	</body>
 </html>
