0c3f0ef92d9a4bc78ae3d073c8fb4bc4caea2207
Eduardo Luna <Lalo@MacBook-Pro-de-Eduardo-7.local>
2017-03-06 20:19:19 -0600

    Fucntionces y comandos

--- Tarea2/BD/tutoring.sql
+++ Tarea2/BD/tutoring.sql
@@ -30,6 +30,42 @@ COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 SET search_path = public, pg_catalog;
 
+--
+-- Name: appointment_insert(); Type: FUNCTION; Schema: public; Owner: Lalo
+--
+
+CREATE FUNCTION appointment_insert() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    INSERT INTO appointment_register(appointment, date) 
+    VALUES(NEW.id, NOW());
+
+    RETURN NEW;
+END;
+$$;
+
+
+ALTER FUNCTION public.appointment_insert() OWNER TO "Lalo";
+
+--
+-- Name: how_many_appointments(character varying, character varying); Type: FUNCTION; Schema: public; Owner: Lalo
+--
+
+CREATE FUNCTION how_many_appointments(teacher_name character varying, teacher_last_name character varying) RETURNS integer
+    LANGUAGE plpgsql
+    AS $$
+declare
+total_appointments integer;
+BEGIN
+    SELECT count(*) into total_appointments FROM appointment, teacher WHERE teacher.id = appointment.teacher AND teacher.name = teacher_name AND teacher.last_name = teacher_last_name;
+    RETURN total_appointments;
+END;
+$$;
+
+
+ALTER FUNCTION public.how_many_appointments(teacher_name character varying, teacher_last_name character varying) OWNER TO "Lalo";
+
 SET default_tablespace = '';
 
 SET default_with_oids = false;
@@ -73,6 +109,40 @@ ALTER SEQUENCE appointment_id_seq OWNED BY appointment.id;
 
 
 --
+-- Name: appointment_register; Type: TABLE; Schema: public; Owner: Lalo
+--
+
+CREATE TABLE appointment_register (
+    id integer NOT NULL,
+    appointment integer,
+    date timestamp without time zone
+);
+
+
+ALTER TABLE appointment_register OWNER TO "Lalo";
+
+--
+-- Name: appointment_register_id_seq; Type: SEQUENCE; Schema: public; Owner: Lalo
+--
+
+CREATE SEQUENCE appointment_register_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE appointment_register_id_seq OWNER TO "Lalo";
+
+--
+-- Name: appointment_register_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Lalo
+--
+
+ALTER SEQUENCE appointment_register_id_seq OWNED BY appointment_register.id;
+
+
+--
 -- Name: schedule; Type: TABLE; Schema: public; Owner: Lalo
 --
 
@@ -81,7 +151,11 @@ CREATE TABLE schedule (
     begin_hour time without time zone,
     end_hour time without time zone,
     schedule_type integer,
-    semester integer
+    semester integer,
+    subject integer,
+    visible boolean,
+    CONSTRAINT schedule_begin_hour_check CHECK ((begin_hour > '06:00:00'::time without time zone)),
+    CONSTRAINT schedule_end_hour_check CHECK ((end_hour > '07:00:00'::time without time zone))
 );
 
 
@@ -215,7 +289,8 @@ ALTER SEQUENCE semester_type_id_seq OWNED BY semester_type.id;
 CREATE TABLE student (
     id integer NOT NULL,
     name character varying(60),
-    last_name character varying(60)
+    last_name character varying(60),
+    password character varying(60)
 );
 
 
@@ -282,7 +357,8 @@ ALTER SEQUENCE subject_id_seq OWNED BY subject.id;
 CREATE TABLE teacher (
     id integer NOT NULL,
     name character varying(60),
-    last_name character varying(60)
+    last_name character varying(60),
+    password character varying(60)
 );
 
 
@@ -344,6 +420,41 @@ ALTER SEQUENCE teacher_schedule_id_seq OWNED BY teacher_schedule.id;
 
 
 --
+-- Name: users; Type: TABLE; Schema: public; Owner: Lalo
+--
+
+CREATE TABLE users (
+    id integer NOT NULL,
+    username character varying(100),
+    password character varying(100),
+    created_at date DEFAULT ('now'::text)::date NOT NULL
+);
+
+
+ALTER TABLE users OWNER TO "Lalo";
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: Lalo
+--
+
+CREATE SEQUENCE users_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE users_id_seq OWNER TO "Lalo";
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Lalo
+--
+
+ALTER SEQUENCE users_id_seq OWNED BY users.id;
+
+
+--
 -- Name: view_appointment; Type: VIEW; Schema: public; Owner: Lalo
 --
 
@@ -399,6 +510,13 @@ ALTER TABLE view_semester OWNER TO "Lalo";
 --
 
 ALTER TABLE ONLY appointment ALTER COLUMN id SET DEFAULT nextval('appointment_id_seq'::regclass);
+
+
+--
+-- Name: appointment_register id; Type: DEFAULT; Schema: public; Owner: Lalo
+--
+
+ALTER TABLE ONLY appointment_register ALTER COLUMN id SET DEFAULT nextval('appointment_register_id_seq'::regclass);
 
 
 --
@@ -458,6 +576,13 @@ ALTER TABLE ONLY teacher_schedule ALTER COLUMN id SET DEFAULT nextval('teacher_s
 
 
 --
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: Lalo
+--
+
+ALTER TABLE ONLY users ALTER COLUMN id SET DEFAULT nextval('users_id_seq'::regclass);
+
+
+--
 -- Data for Name: appointment; Type: TABLE DATA; Schema: public; Owner: Lalo
 --
 
@@ -468,6 +593,7 @@ COPY appointment (id, student, schedule, teacher, subject, topic, day) FROM stdi
 4	2	14	7	5	Maps in Xcode	2017-02-16
 5	6	5	5	4	Testing java	2017-02-10
 6	5	11	6	3	Vectorial Spaces	2017-02-10
+7	1	1	1	1	Problemas de matematicas	2017-05-05
 \.
 
 
@@ -475,29 +601,45 @@ COPY appointment (id, student, schedule, teacher, subject, topic, day) FROM stdi
 -- Name: appointment_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Lalo
 --
 
-SELECT pg_catalog.setval('appointment_id_seq', 6, true);
+SELECT pg_catalog.setval('appointment_id_seq', 7, true);
+
+
+--
+-- Data for Name: appointment_register; Type: TABLE DATA; Schema: public; Owner: Lalo
+--
+
+COPY appointment_register (id, appointment, date) FROM stdin;
+1	7	2017-03-06 20:09:57.46384
+\.
+
+
+--
+-- Name: appointment_register_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Lalo
+--
+
+SELECT pg_catalog.setval('appointment_register_id_seq', 1, true);
 
 
 --
 -- Data for Name: schedule; Type: TABLE DATA; Schema: public; Owner: Lalo
 --
 
-COPY schedule (id, begin_hour, end_hour, schedule_type, semester) FROM stdin;
-1	11:00:00	12:30:00	1	3
-2	12:30:00	14:00:00	2	3
-3	14:00:00	15:30:00	3	3
-4	08:00:00	09:30:00	1	3
-5	10:00:00	11:30:00	2	3
-6	16:00:00	17:30:00	3	3
-7	17:00:00	18:30:00	1	3
-8	09:00:00	10:30:00	2	3
-9	10:30:00	12:30:00	3	3
-10	07:00:00	08:30:00	1	3
-11	08:30:00	10:00:00	2	3
-12	18:00:00	17:30:00	3	3
-13	14:00:00	15:30:00	1	3
-14	17:00:00	18:30:00	2	3
-15	10:00:00	11:30:00	3	3
+COPY schedule (id, begin_hour, end_hour, schedule_type, semester, subject, visible) FROM stdin;
+1	11:00:00	12:30:00	1	3	2	t
+2	12:30:00	14:00:00	2	3	2	t
+3	14:00:00	15:30:00	3	3	2	t
+4	08:00:00	09:30:00	1	3	2	t
+5	10:00:00	11:30:00	2	3	2	t
+6	16:00:00	17:30:00	3	3	2	t
+7	17:00:00	18:30:00	1	3	2	t
+8	09:00:00	10:30:00	2	3	2	t
+9	10:30:00	12:30:00	3	3	2	t
+10	07:00:00	08:30:00	1	3	2	t
+11	08:30:00	10:00:00	2	3	2	t
+12	18:00:00	17:30:00	3	3	2	t
+13	14:00:00	15:30:00	1	3	2	t
+14	17:00:00	18:30:00	2	3	2	t
+15	10:00:00	11:30:00	3	3	2	t
 \.
 
 
@@ -566,14 +708,16 @@ SELECT pg_catalog.setval('semester_type_id_seq', 3, true);
 -- Data for Name: student; Type: TABLE DATA; Schema: public; Owner: Lalo
 --
 
-COPY student (id, name, last_name) FROM stdin;
-1	Eduardo	Luna
-2	Jota Pe	Flores
-3	Joel	Ramirez
-4	Jesus	Miron
-5	Julian	Huerta
-6	Enrique	Lozada
-7	Alehandro	Tovar
+COPY student (id, name, last_name, password) FROM stdin;
+1	Eduardo	Luna	secret
+2	Jota Pe	Flores	secret
+3	Joel	Ramirez	secret
+4	Jesus	Miron	secret
+5	Julian	Huerta	secret
+6	Enrique	Lozada	secret
+7	Alejandro	Tovar	secret
+8	Fany	Pitol	secret
+9	Fernanda	Montanio	secret
 \.
 
 
@@ -581,7 +725,7 @@ COPY student (id, name, last_name) FROM stdin;
 -- Name: student_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Lalo
 --
 
-SELECT pg_catalog.setval('student_id_seq', 7, true);
+SELECT pg_catalog.setval('student_id_seq', 9, true);
 
 
 --
@@ -609,14 +753,14 @@ SELECT pg_catalog.setval('subject_id_seq', 6, true);
 -- Data for Name: teacher; Type: TABLE DATA; Schema: public; Owner: Lalo
 --
 
-COPY teacher (id, name, last_name) FROM stdin;
-1	Manuel	Calleros
-2	Daniel	Perez
-3	Alvaro	Andrade
-4	Avel	Amado
-5	Gilberto	Hernandez
-6	Erika	Ramirez
-7	Soledad	Rios
+COPY teacher (id, name, last_name, password) FROM stdin;
+1	Manuel	Calleros	secret
+2	Daniel	Perez	secret
+3	Alvaro	Andrade	secret
+5	Gilberto	Hernandez	secret
+6	Erika	Ramirez	secret
+7	Soledad	Rios	secret
+4	Abel	Odiado	secret
 \.
 
 
@@ -664,11 +808,35 @@ SELECT pg_catalog.setval('teacher_schedule_id_seq', 21, true);
 
 
 --
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: Lalo
+--
+
+COPY users (id, username, password, created_at) FROM stdin;
+1	user0	secret	2017-03-03
+\.
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Lalo
+--
+
+SELECT pg_catalog.setval('users_id_seq', 1, true);
+
+
+--
 -- Name: appointment appointment_pkey; Type: CONSTRAINT; Schema: public; Owner: Lalo
 --
 
 ALTER TABLE ONLY appointment
     ADD CONSTRAINT appointment_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: appointment_register appointment_register_pkey; Type: CONSTRAINT; Schema: public; Owner: Lalo
+--
+
+ALTER TABLE ONLY appointment_register
+    ADD CONSTRAINT appointment_register_pkey PRIMARY KEY (id);
 
 
 --
@@ -736,6 +904,29 @@ ALTER TABLE ONLY teacher_schedule
 
 
 --
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: Lalo
+--
+
+ALTER TABLE ONLY users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: appointment register_appointment; Type: TRIGGER; Schema: public; Owner: Lalo
+--
+
+CREATE TRIGGER register_appointment AFTER INSERT ON appointment FOR EACH ROW EXECUTE PROCEDURE appointment_insert();
+
+
+--
+-- Name: appointment_register appointment_register_appointment_fkey; Type: FK CONSTRAINT; Schema: public; Owner: Lalo
+--
+
+ALTER TABLE ONLY appointment_register
+    ADD CONSTRAINT appointment_register_appointment_fkey FOREIGN KEY (appointment) REFERENCES appointment(id);
+
+
+--
 -- Name: appointment appointment_schedule_fkey; Type: FK CONSTRAINT; Schema: public; Owner: Lalo
 --
 
@@ -781,6 +972,14 @@ ALTER TABLE ONLY schedule
 
 ALTER TABLE ONLY schedule
     ADD CONSTRAINT schedule_semester_fkey FOREIGN KEY (semester) REFERENCES semester(id);
+
+
+--
+-- Name: schedule schedule_subject_fkey; Type: FK CONSTRAINT; Schema: public; Owner: Lalo
+--
+
+ALTER TABLE ONLY schedule
+    ADD CONSTRAINT schedule_subject_fkey FOREIGN KEY (subject) REFERENCES schedule(id);
 
 
 --
